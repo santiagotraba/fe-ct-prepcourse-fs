7919af48bd8840144e842bf031915f360045378b
santiagotraba <santiagontraba95@gmail.com>
2023-08-08 20:41:56 -0300

    ejercicios del modulo 07

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -2,31 +2,51 @@
 /*1️⃣ EJERCICIO 01 1️⃣*/
 
 function crearUsuario() {
-   // Crea una Clase de ES6 o una función constructora llamada "Usuario".
-   // El constructor de esta clase debe tener las propiedades: "usuario", "nombre", "email" y "password".
-   // El valor de cada propiedad la recibirá por parámetro.
-   // Además, esta clase debe tener un método llamado "saludar".
-   // Este método debe retornar el string: 'Hola, mi nombre es {{nombre}}'.
-   // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
-   // Retornar la clase.
-   // Tu código:
+    // Crea una Clase de ES6 o una función constructora llamada "Usuario".
+    // El constructor de esta clase debe tener las propiedades: "usuario", "nombre", "email" y "password".
+    // El valor de cada propiedad la recibirá por parámetro.
+    // Además, esta clase debe tener un método llamado "saludar".
+    // Este método debe retornar el string: 'Hola, mi nombre es {{nombre}}'.
+    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
+    // Retornar la clase.
+    // Tu código:
+    class Usuario {
+        constructor(usuario, nombre, email, password) {
+            this.usuario = usuario;
+            this.nombre = nombre;
+            this.email = email;
+            this.password = password;
+        }
+        saludar() {
+            return "Hola, mi nombre es " + this.nombre;
+        }
+    }
+    return Usuario;
 }
 
 function agregarMetodoPrototype(Usuario) {
-   // Agrega un método al prototipo de "Usuario".
-   // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
-   // Tu código:
+    // Agrega un método al prototipo de "Usuario".
+    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
+    // Tu código:
+    Usuario.prototype.saludar = function () {
+        return "Hello World!";
+    };
 }
 
 function agregarStringInvertida() {
-   // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
-   // El método debe llamarse "reverse".
-   // [PISTA]: necesitarás utilizar el objeto "this".
+    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
+    // El método debe llamarse "reverse".
+    // [PISTA]: necesitarás utilizar el objeto "this".
+    if (!String.prototype.reverse) {
+        String.prototype.reverse = function () {
+            return this.split("").reverse().join("");
+        };
+    }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   crearUsuario,
-   agregarMetodoPrototype,
-   agregarStringInvertida,
+    crearUsuario,
+    agregarMetodoPrototype,
+    agregarStringInvertida,
 };
